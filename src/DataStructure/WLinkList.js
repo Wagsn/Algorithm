ed8334eea9095cf047e901a6830c9b5b07d6cca5
Wagsn <33355908+Wagsn@users.noreply.github.com>
2018-04-11 17:24:29 +0800

    Update WLinkList.js

--- src/DataStructure/WLinkList.js
+++ src/DataStructure/WLinkList.js
@@ -1,6 +1,30 @@
 // link list 
-
-class LinkList {
+// hold data for Linked Storage Structure
+class Node {
+    constructor(data){
+        this.data = data;
+    }
+}
+class SNode{
+    constructor(data, next){
+        this.data = data
+        thus.next = data;
+    }
+}
+class DNode{
+    constructor(data, prev, next){
+        this.data = data || null;
+        this.prev = prev || null;
+        this.next = next || null;
+    }
+}
+class LinkedList {
+    
+}
+class SinglyLinkList {
+    
+}
+class DoublyLinkList {
     constructor() {
         this.first = null;
         this.last = null;
