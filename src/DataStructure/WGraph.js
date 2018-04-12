d3030e25deb78119a8a95a8ad68851ac3ba643d8
Wagsn <33355908+Wagsn@users.noreply.github.com>
2018-04-12 22:31:38 +0800

    update

--- src/DataStructure/WGraph.js
+++ src/DataStructure/WGraph.js
@@ -1,5 +1,31 @@
 // WGraph.js
 
-function WGraph(){
-    //
+// vertex for graph
+class WS_Vertex {
+    constructor() {
+        //
+        this.weight = 0;
+        this.data = null;
+    }
 }
+function WS_Ege(){
+    //
+    this.weight =0;
+
+}
+// Graph abstract data type
+class WS_Graph {
+    constructor() {
+        //
+        this.datas =new Array()
+        this.vertexs = [];
+        this.eges = [];
+        this.add = function addVertex(vertex) {
+            this.datas.
+            if (this.vertexs.indexOf(vertex) == -1) {
+                this.vertexs.push(vertex);
+            }
+        };
+    }
+}
+
