da63835a6aadb9226e32a345d3dfed6cb554580e
Wagsn <33355908+Wagsn@users.noreply.github.com>
2018-04-13 15:59:26 +0800

    update

--- src/DataStructure/WQueue.js
+++ src/DataStructure/WQueue.js
@@ -1,6 +1,13 @@
 // WQueue.js
-
 // a queue that first-in-first-out follow the list 
-function Queue(){
-    //
+class WS_Queue {
+    dataStore =[];
+    constructor() {
+    }
+    deQueue () {
+    }
+    enQueue (item) {
+    }
+    traverse (callback){}
 }
+
