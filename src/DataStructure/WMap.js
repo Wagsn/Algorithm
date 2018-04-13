7d8ca62851f0ea19e171a7440613e3f101e788e9
Wagsn <33355908+Wagsn@users.noreply.github.com>
2018-04-13 11:51:38 +0800

    Update WMap.js

--- src/DataStructure/WMap.js
+++ src/DataStructure/WMap.js
@@ -1,5 +1,31 @@
 // WMap.js 字典 映射 键值对数组
 
 function Map(){
-    this.dataStore = null;
+    this.dataStore = [];
+}
+
+function MAdd(key, value){
+    this.dataStore[key] = value;
+}
+
+function MRemove(key){
+    delete this.dataStore[key];
+}
+
+function MSize() {
+    return this.dataStore.length;
+}
+
+function MToString() {
+    var data = this.dataStore;
+    var strHtml = Object.keys(data).sort().map(function (key) {
+        return "key:" + key + ",value:" + data[key];
+    }).join("|");
+    return strHtml;
+}
+
+function MContains(key) {
+    return Object.keys(this.dataStore).some(function (keyName) {
+        return keyName == key;
+    });
 }
