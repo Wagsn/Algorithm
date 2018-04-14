599ec4e741d375351341e850052fece3549a3284
Wagsn <33355908+Wagsn@users.noreply.github.com>
2018-04-14 11:31:03 +0800

    Update WS_Stack.js

--- src/DataStructure/WS_Stack.js
+++ src/DataStructure/WS_Stack.js
@@ -1,5 +1,17 @@
 // WS_Stack.js
 
-class Stack{
+// fist-in-last-out 
+class WS_Stack{
     //
+    constructor(){
+        this._dataStore = [];
+    }
+    // remove last element and return it
+    pop(){
+        return this._dataStore.pop();
+    }
+    // appends element, return the length of stack
+    push(item){
+        return this._dataStore.push(item);
+    }
 }
